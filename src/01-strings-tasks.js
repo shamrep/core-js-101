cbc8d32a78dfb14856351115bfdca6a4690d286f
shamrep <shamrep@gmail.com>
2023-10-26 22:41:49 +0300

    test3

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -52,7 +52,7 @@ function getStringLength(value) {
  *   'Chuck','Norris'  => 'Hello, Chuck Norris!'
  */
 function getStringFromTemplate(firstName, lastName) {
- return 'Hello, ${firstName} ${lastName}!';
+  return `Hello, ${firstName} ${lastName}!`;
 }
 
 /**
